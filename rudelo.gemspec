c735671bba991ccb763b112a0f163df96a346a0e
Michael Johnston <lastobelus@mac.com>
2013-05-15 15:07:38 -0700

    add parslet and setup rspec for parser tests

--- rudelo.gemspec
+++ rudelo.gemspec
@@ -19,6 +19,7 @@ Gem::Specification.new do |spec|
   spec.require_paths = ["lib"]
 
   spec.add_dependency  "rufus-decision"
+  spec.add_dependency  "parslet"
   spec.add_development_dependency "bundler", "~> 1.3"
   spec.add_development_dependency "rake"
   spec.add_development_dependency "rspec"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,18 @@
+
 require 'rubygems'
 require 'bundler/setup'
 
 require 'rudelo'
 
 RSpec.configure do |config|
-  # some (optional) config here
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+
+  config.order = 'random'
+
 end
+
+require "parslet"
+require "parslet/rig/rspec"
+require 'parslet/convenience'

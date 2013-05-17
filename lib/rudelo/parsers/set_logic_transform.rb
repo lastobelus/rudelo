ad8cdd2d30fb648f5e7b8bbafb75f1b4b90f6466
Michael Johnston <lastobelus@mac.com>
2013-05-16 20:18:39 -0700

    transform cardinality expression
    corrected a misunderstanding I had about transforms along the way:
    
    the rule (a: simple(:a)) won't match {a: blah, b: foo}
    you need a rule (a: simple(:a), b: simple(:b))

--- lib/rudelo/parsers/set_logic_transform.rb
+++ lib/rudelo/parsers/set_logic_transform.rb
@@ -4,30 +4,43 @@ module Rudelo
   module Parsers
 
     class SetLogicTransform < Rudelo::Parsers::SetValueTransform
+    # class SetValueTransform < Parslet::Transform
       CardinalityExpr = Struct.new(:op, :qty) {
         def eval(set)
           set.size.send op, qty
         end
       }
 
-      rule(op: simple(:op)){
+      rule(cardinality_expression: subtree(:expr)){
+        CardinalityExpr.new(expr[:op], expr[:qty])
+      }
+
+      def self.translate_op(op)
         case op
-        when '=', 'cardinality-equals';                    :"=="
-        when 'cardinality-less-than', 'proper-subset';     :<
-        when 'cardinality-greater-than', 'proper-superset';:>
-        when 'subset';                                     :<=
-        when 'superset';                                   :>=
-        when 'intersection';                               :&
-        when 'union';                                      :+
-        when 'difference';                                 :-
-        when 'exclusive';                                  :'^'
+        when '#=', 'cardinality-equals';       :"=="
+        when '#<','cardinality-less-than',
+          'proper-subset';                     :<
+        when '#>','cardinality-greater-than',
+          'proper-superset';                   :>
+        when 'subset';                         :<=
+        when 'superset';                       :>=
+        when 'intersection';                   :&
+        when 'union';                          :+
+        when 'difference';                     :-
+        when 'exclusive';                      :'^'
         else
           op.to_sym
         end
+      end
+
+      rule(op: simple(:op)){
+        SetLogicTransform.translate_op(op)
       }
 
-      rule(cardinality_expression: subtree(:expr)){
-        CardinalityExpr.new(expr[:op], expr[:quantity])
+      rule(qty: simple(:qty)){ qty.to_i }
+
+      rule(op: simple(:op), qty: simple(:qty)){ 
+        {op: SetLogicTransform.translate_op(op), qty: qty.to_i}
       }
     end
   end
--- spec/parsers/set_logic_transform_spec.rb
+++ spec/parsers/set_logic_transform_spec.rb
@@ -9,11 +9,12 @@ describe "Rudelo::Parsers::SetLogicTransform" do
 
   context "ops" do
     specify{expect(xform.apply(:op => "+")).to eq(:"+")}
-    specify{expect(xform.apply(:op => "=")).to eq(:"==")}
+    specify{expect(xform.apply(:op => "#=")).to eq(:"==")}
     specify{expect(xform.apply(:op => "cardinality-equals")).to eq(:"==")}
-    specify{expect(xform.apply(:op => ">")).to eq(:>)}
+    specify{expect(xform.apply(:op => "#>")).to eq(:>)}
     specify{expect(xform.apply(:op => "cardinality-greater-than")).to eq(:>)}
     specify{expect(xform.apply(:op => "proper-superset")).to eq(:>)}
+
   end
 
   context "CardinalityExpr" do
@@ -23,5 +24,33 @@ describe "Rudelo::Parsers::SetLogicTransform" do
     specify{ expect(subject.new(:<, 2).eval(set)).to be_false }
     specify{ expect(subject.new(:==, 2).eval(set)).to be_false }
     specify{ expect(subject.new(:==, 3).eval(set)).to be_true }
+
+    it "should transform cardinality_expressions" do
+      puts
+      pp parser.parse("#> 2") 
+      puts
+      pp xform.apply( parser.parse("#> 2") )
+      puts
+      expect(
+        xform.apply( 
+          parser.parse("#> 2") 
+        )[:cardinality_match]).to be_a_kind_of(
+      Rudelo::Parsers::SetLogicTransform::CardinalityExpr)
+
+      expect(
+        xform.apply( 
+          parser.parse("#> 2") 
+        )[:cardinality_match].eval(set)
+      ).to be_true
+
+      expect(
+        xform.apply(
+          parser.parse("#< 2")
+        )[:cardinality_match].eval(set)
+      ).to be_false
+    end
   end
+
+
+
 end
